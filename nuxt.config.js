7fc1c1a5b221553876589bdd1a87857795173b5e
Dmitry Nevgod <85366430+DmitriyNevgod@users.noreply.github.com>
2022-10-26 04:57:12 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ module.exports = {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.API_URL,
+    baseURL: 'https://frontend-test-assignment-api.abz.agency/api/v1/',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
